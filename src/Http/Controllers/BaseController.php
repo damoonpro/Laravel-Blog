4d0f8c3275705221f48dde698b9a7fec34946147
matio-nora <matio.nora2000@gmail.com>
2022-08-16 16:42:09 +0430

    feat-controller: base controller sets

---
+++ src/Http/Controllers/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Damoon\Blog\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
